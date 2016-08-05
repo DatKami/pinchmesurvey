c54bc92a0e031cee4ffe0ed061554154868f8c4b
DatKami <studiowolf8@gmail.com>
2016-08-05 16:01:15 -0400

    Responses now allow text responses and block them when they aren't needed.

--- app/controllers/responses_controller.rb
+++ app/controllers/responses_controller.rb
@@ -1,2 +1,25 @@
 class ResponsesController < ApplicationController
+	def show
+		@response = Response.find(params[:id])
+	end
+	
+	def edit 
+		@response = Response.find(params[:id])
+	end
+	
+	def update
+		@response = Response.find(params[:id])
+		
+		if @response.update(response_params)
+			redirect_to @response.submission
+		else
+			render 'edit'
+			#not done
+		end
+	end
+	
+	private
+		def response_params
+			params.require(:response).permit(:text_response)
+		end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,10 @@ Rails.application.routes.draw do
 	resources :answers
 	resources :submissions do
 		resources :surveys
+		resources :responses
+	end
+	resources :responses do
+		resources :questions
 	end
 	root 'surveys#index'
 	# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
