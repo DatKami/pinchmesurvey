f2db474f514d3d14c9d82826f9c9f180244e5e4d
DatKami <studiowolf8@gmail.com>
2016-08-06 14:18:08 -0400

    Added ability to respond to checkboxes. Also handles case where no checkboxes are checked.

--- app/controllers/responses_controller.rb
+++ app/controllers/responses_controller.rb
@@ -1,21 +1,27 @@
 class ResponsesController < ApplicationController
   def show
     @response = Response.find(params[:id])
+
+    redirect_to edit_response_path
   end
     def edit 
     @response = Response.find(params[:id])
   end
     def update
+    #we need to provide an empty array incase the post doesn't, to force an update
+    params[:response][:answer_ids] ||= [] 
     @response = Response.find(params[:id])
-        if @response.update(response_params)
+
+    if @response.update(response_params)
       redirect_to @response.submission
     else
       render 'edit'
       #not done
     end
   end
-    private
+
+  private
     def response_params
-      params.require(:response).permit(:text_response, :answer_id)
+      params.require(:response).permit(:text_response, :answer_id, :answer_ids => [])
     end
 end
