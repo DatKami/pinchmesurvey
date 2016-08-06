a1fb6b0e25f7b6a1dd4facda8841c2ffe1784f12
DatKami <studiowolf8@gmail.com>
2016-08-06 13:23:10 -0400

    Added ability to answer drop down questions.

--- app/controllers/responses_controller.rb
+++ app/controllers/responses_controller.rb
@@ -1,25 +1,21 @@
 class ResponsesController < ApplicationController
-	def show
-		@response = Response.find(params[:id])
-	end
-	
-	def edit 
-		@response = Response.find(params[:id])
-	end
-	
-	def update
-		@response = Response.find(params[:id])
-		
-		if @response.update(response_params)
-			redirect_to @response.submission
-		else
-			render 'edit'
-			#not done
-		end
-	end
-	
-	private
-		def response_params
-			params.require(:response).permit(:text_response)
-		end
+  def show
+    @response = Response.find(params[:id])
+  end
+    def edit 
+    @response = Response.find(params[:id])
+  end
+    def update
+    @response = Response.find(params[:id])
+        if @response.update(response_params)
+      redirect_to @response.submission
+    else
+      render 'edit'
+      #not done
+    end
+  end
+    private
+    def response_params
+      params.require(:response).permit(:text_response, :answer_id)
+    end
 end
--- app/models/response.rb
+++ app/models/response.rb
@@ -1,5 +1,6 @@
 class Response < ApplicationRecord
 	belongs_to :submission
 	has_and_belongs_to_many :answers
+	accepts_nested_attributes_for :answers
 	belongs_to :question
 end
