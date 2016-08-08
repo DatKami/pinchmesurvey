fe15dd673d502860c76e22faa270e1ebd9c9ce2d
DatKami <studiowolf8@gmail.com>
2016-08-08 09:16:46 -0400

    Cleaned survey controllers

--- app/controllers/surveys_controller.rb
+++ app/controllers/surveys_controller.rb
@@ -1,54 +1,51 @@
+# Public: A handler for surveys.
 class SurveysController < ApplicationController
+  def index
+    @surveys = Survey.all
+  end
 
-	#index, show, new, edit, create, update and destroy
-	#place before private or protected methods
+  def show
+    @survey = Survey.find(params[:id])
+  end
 
-	def index
-		@surveys = Survey.all
-	end
-	
-	def show
-		@survey = Survey.find(params[:id])
-	end
+  def new
+    @survey = Survey.new
+  end
 
-	def new
-		@survey = Survey.new
-	end
-	
-	def edit
-		@survey = Survey.find(params[:id])
-	end
-	
-	def create
-		@survey = Survey.new(survey_params)
-		
-		if @survey.save
-			redirect_to @survey
-		else
-			render 'new'
-		end
-	end
-	
-	def update
-		@survey = Survey.find(params[:id])
-		
-		if @survey.update(survey_params)
-			redirect_to @survey
-		else
-			render 'edit'
-		end
-	end
-	
-	def destroy
-		@survey = Survey.find(params[:id])
-		@survey.destroy
-		
-		redirect_to surveys_path
-	end
-	
-	private
-		def survey_params
-			params.require(:survey).permit(:title)
-		end
+  def edit
+    @survey = Survey.find(params[:id])
+  end
 
+  def create
+    @survey = Survey.new(survey_params)
+
+    if @survey.save
+      redirect_to @survey
+    else
+      render 'new'
+    end
+  end
+
+  def update
+    @survey = Survey.find(params[:id])
+
+    if @survey.update(survey_params)
+      redirect_to @survey
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @survey = Survey.find(params[:id])
+    @survey.destroy
+
+    redirect_to surveys_path
+  end
+
+  private
+
+  def survey_params
+    params.require(:survey).permit(:title)
+  end
 end
